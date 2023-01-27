02d3d495c9472fe99941c7db9067cce15f999c14
Jason White <jmw5598@gmail.com>
2023-01-27 16:52:47 -0500

    initial scaffold of angular 15 monorepo

---
+++ clients/projects/admin/src/main.ts
@@ -0,0 +1,13 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
+
+import { AppComponent } from './app/app.component';
+import { appRoutes } from './app/app.routing';
+
+bootstrapApplication(
+  AppComponent, {
+    providers: [
+      provideRouter(appRoutes)
+    ]
+  }
+).catch(err => console.error(err));

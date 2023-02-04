dad11bc7c52be70c4b0114f548e4414dffa6da53
Jason White <jmw5598@gmail.com>
2023-02-04 05:58:55 -0500

    reworked database identity entities and created migrations

--- server/database/migrations/1675278435804-seed-roles-tables.ts
+++ server/database/migrations/1675278435804-seed-roles-tables.ts
@@ -1,21 +1,24 @@
-import { MigrationInterface, QueryRunner } from "typeorm"
+import { MigrationInterface, QueryRunner } from 'typeorm';
 import { Role, RoleTypes } from '../../libs/common/src';
 
-export class seedsRoleTable1675094157260 implements MigrationInterface {
-    name = 'seedsRoleTable1675094157260';
+export class seedRolesTables1675278435804 implements MigrationInterface {
+    name = 'seedRolesTables1675278435804';
 
     private _roles: Partial<Role>[] = [
         {
+            id: '56b04d04-b6f6-4997-9abb-bf608a2efb3e',
             deletedAt: null,
             name: RoleTypes.ROOT,
             claims: null
         },
         {
+            id: 'a2d8fbb3-281e-431f-9228-e2fa9e5267f3',
             deletedAt: null,
             name: RoleTypes.ADMIN,
             claims: null
         },
         {
+            id: '71cde4d8-db76-41cd-8944-b9529efe38bb',
             deletedAt: null,
             name: RoleTypes.USER,
             claims: null
@@ -30,6 +33,6 @@ export class seedsRoleTable1675094157260 implements MigrationInterface {
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DELETE * FROM role`);
+        await queryRunner.query(`DELETE * FROM app_role`);
     }
 }
